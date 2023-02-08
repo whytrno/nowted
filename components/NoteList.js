37139cb5ce9cc3b90250b42814f220a51caac8ae
Wahyu <admin@lazy2code.my.id>
2023-02-08 14:17:09 +0700

    Fix side bar height issue

--- components/NoteList.js
+++ components/NoteList.js
@@ -11,9 +11,9 @@ export default function NoteList({ activeFolder, activeNote, setActiveNote, user
 
     return (
         // 2xl:w-[20.833333%] sm:w-[30%]
-        <div className="bg-secondary pt-[30px] space-y-[30px] px-[20px] 2xl:col-span-2 sm:col-span-3 max-h-screen grid grid-rows-12">
-            <h3 className="text-xl font-semibold row-span-1">{folderName}</h3>
-            <div className="space-y-[20px] overflow-auto row-span-11">
+        <div className="bg-secondary pt-[30px] space-y-[30px] px-[20px] 2xl:col-span-2 sm:col-span-3 min-h-screen flex flex-col">
+            <h3 className="text-xl font-semibold">{folderName}</h3>
+            <div className="space-y-[20px] h-[75%] border overflow-auto">
                 {notes.map(note => (
                     <Note key={note._id} {...note} setActiveNote={setActiveNote} activeNote={activeNote} />
                 ))}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -49,62 +49,63 @@ export default function Sidebar({ activeFolder, setActiveFolder, userData, setAc
 
     return (
         // 2xl:w-[20.833333%] sm:w-[30%]
-        // <div className="gap-[30px] max-h-screen justify-between py-[30px] 2xl:col-span-2 sm:col-span-3 grid grid-rows-12"></div>
-        <div className="grid grid-rows-12 gap-[30px] py-[20px] max-h-screen 2xl:col-span-2 sm:col-span-3">
-            <div className="flex justify-between px-[20px] items-center row-span-1">
-                <h1 className="font-kaushan flex text-logo gap-[10px]">
-                    Nowted
-                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g clipPath="url(#clip0_18_204)">
-                            <path d="M10.625 1.87501C10.7892 1.71085 10.984 1.58064 11.1985 1.4918C11.413 1.40296 11.6429 1.35724 11.875 1.35724C12.1071 1.35724 12.337 1.40296 12.5515 1.4918C12.766 1.58064 12.9608 1.71085 13.125 1.87501C13.2892 2.03916 13.4194 2.23404 13.5082 2.44851C13.597 2.66299 13.6428 2.89286 13.6428 3.12501C13.6428 3.35715 13.597 3.58703 13.5082 3.8015C13.4194 4.01598 13.2892 4.21085 13.125 4.37501L4.6875 12.8125L1.25 13.75L2.1875 10.3125L10.625 1.87501Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                        </g>
-                        <defs>
-                            <clipPath id="clip0_18_204">
-                                <rect width="15" height="15" fill="white" />
-                            </clipPath>
-                        </defs>
-                    </svg>
-                </h1>
-                <SearchIcon setSearchActive={setSearchActive} searchActive={searchActive} />
-            </div>
-            {searchActive ? (
-                <div className="px-[20px] transition-all row-span-1">
-                    <div className="px-[10px] flex gap-[8px] bg-white/5 rounded-[3px]">
-                        <SearchIcon />
-                        <input type="text" placeholder="Search Note" className="py-[10px] bg-transparent text-white/60 font-semibold text-md" />
-                    </div>
-                </div>
-            ) : (
-                <div className="px-[20px] row-span-1">
-                    <button onClick={addNoteHandler} className="flex gap-[8px] w-full p-[10px] items-center justify-center bg-white/5 rounded-[3px]">
-                        <PlusIcon />
-                        New Note
-                    </button>
-                </div>
-            )}
-            <div className="row-span-3 space-y-[8px]">
-                <h4 className="font-semibold text-sm text-white/60 px-[20px]">Recents</h4>
-                {recents.map(recent => (
-                    <Recent key={recent.title} {...recent} />
-                ))}
-            </div>
-            <div className="row-span-5 overflow-auto space-y-[8px]">
+        <div className="flex flex-col gap-[30px] max-h-screen justify-between py-[30px] 2xl:col-span-2 sm:col-span-3">
+            <div className="space-y-[30px]">
                 <div className="flex justify-between px-[20px]">
-                    <h4 className="font-semibold text-sm text-white/60">Folders</h4>
-                    <button>
-                        <FolderPlusIcon setNewFolderActive={setNewFolderActive} newFolderActive={newFolderActive} />
-                    </button>
+                    <h1 className="font-kaushan flex text-logo gap-[10px]">
+                        Nowted
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clipPath="url(#clip0_18_204)">
+                                <path d="M10.625 1.87501C10.7892 1.71085 10.984 1.58064 11.1985 1.4918C11.413 1.40296 11.6429 1.35724 11.875 1.35724C12.1071 1.35724 12.337 1.40296 12.5515 1.4918C12.766 1.58064 12.9608 1.71085 13.125 1.87501C13.2892 2.03916 13.4194 2.23404 13.5082 2.44851C13.597 2.66299 13.6428 2.89286 13.6428 3.12501C13.6428 3.35715 13.597 3.58703 13.5082 3.8015C13.4194 4.01598 13.2892 4.21085 13.125 4.37501L4.6875 12.8125L1.25 13.75L2.1875 10.3125L10.625 1.87501Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_18_204">
+                                    <rect width="15" height="15" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </h1>
+                    <SearchIcon setSearchActive={setSearchActive} searchActive={searchActive} />
                 </div>
-                <div>
-                    {newFolderActive && (
-                        <AddFolder fetchData={fetchData} newFolderActive={newFolderActive} setNewFolderActive={setNewFolderActive} folders={folders} />
-                    )}
-                    {folders.map((folder, index) => (
-                        <Folder key={index} {...folder} setActiveFolder={setActiveFolder} activeFolder={activeFolder} setActiveNote={setActiveNote} />
+                {searchActive ? (
+                    <div className="px-[20px] transition-all">
+                        <div className="px-[10px] flex gap-[8px] bg-white/5 rounded-[3px]">
+                            <SearchIcon />
+                            <input type="text" placeholder="Search Note" className="py-[10px] bg-transparent text-white/60 font-semibold text-md" />
+                        </div>
+                    </div>
+                ) : (
+                    <div className="px-[20px]">
+                        <button onClick={addNoteHandler} className="flex gap-[8px] w-full p-[10px] items-center justify-center bg-white/5 rounded-[3px]">
+                            <PlusIcon />
+                            New Note
+                        </button>
+                    </div>
+                )}
+                <div className="space-y-[8px]">
+                    <h4 className="font-semibold text-sm text-white/60 px-[20px]">Recents</h4>
+                    {recents.map(recent => (
+                        <Recent key={recent.title} {...recent} />
                     ))}
                 </div>
+                <div className="space-y-[8px]">
+                    <div className="flex justify-between px-[20px]">
+                        <h4 className="font-semibold text-sm text-white/60">Folders</h4>
+                        <button>
+                            <FolderPlusIcon setNewFolderActive={setNewFolderActive} newFolderActive={newFolderActive} />
+                        </button>
+                    </div>
+                    <div className="max-h-52 overflow-auto">
+                        {newFolderActive && (
+                            <AddFolder fetchData={fetchData} newFolderActive={newFolderActive} setNewFolderActive={setNewFolderActive} folders={folders} />
+                        )}
+                        {folders.map((folder, index) => (
+                            <Folder key={index} {...folder} setActiveFolder={setActiveFolder} activeFolder={activeFolder} setActiveNote={setActiveNote} />
+                        ))}
+                    </div>
+                </div>
             </div>
-            <div className="row-span-2 space-y-[8px]">
+            <div className="space-y-[8px]">
                 <h4 className="font-semibold text-sm text-white/60 px-[20px]">More</h4>
                 <More setShortcutModal={setShortcutModal} />
             </div>
