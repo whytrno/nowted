8effa806efc9a3f5078b63f1fd7bc4dadcce0266
Wahyu <admin@lazy2code.my.id>
2023-02-08 12:20:35 +0700

    Fix minor issue

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home({ userDataRaw }) {
     <div>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
         <title>Nowted 📖</title>
