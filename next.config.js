4eb6c700ed062dfb35af4282cc220bbef75fc6d5
Wahyu <admin@lazy2code.my.id>
2023-02-10 09:58:31 +0700

    Change to production

--- next.config.js
+++ next.config.js
@@ -6,8 +6,10 @@ const nextConfig = {
     MONGO_URI: 'mongodb+srv://whytrno:Whytrno2205@cluster.d5zofi5.mongodb.net/nowted?retryWrites=true&w=majority',
     GOOGLE_CLIENT_ID: '562184986347-h08ptk9i49uk8egdr6lbr956ol3komtu.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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',
-    GITHUB_ID: 'bb44e4c585a81ace4a70',
-    GITHUB_SECRET: '2aeae2b6b5ab5cca76bda1f957d07604dda4a70a',
+    // GITHUB_ID: 'bb44e4c585a81ace4a70',
+    // GITHUB_SECRET: '2aeae2b6b5ab5cca76bda1f957d07604dda4a70a',
+    GITHUB_ID: '476589d667a009c03789',
+    GITHUB_SECRET: 'b644109b002c03298604d46bf887f048fdd6b71e',
     SECRET: 'e0d4f088d20db90bc4819af49a2e7b7a'
   },
   //webpack alias
